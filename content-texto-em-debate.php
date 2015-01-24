dfdb5d3214cfd37c8cbec177afbbf93327ab8317
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-01-24 19:19:03 -0200

    finalização dos templates do texto de debate

--- content-texto-em-debate.php
+++ content-texto-em-debate.php
@@ -1,22 +1,25 @@
-<div class="container">
-	<div class="row">
-		<div class="col-xs-12">
-			<?php get_template_part("menu", "horizontal"); ?>
+<div class="mt-md row">
+	<div class="col-md-3">
+		<?php get_template_part("menu", "indice"); ?>
+		<div class="mt-md" id="fixa-menu-eixo">
+			<?php get_template_part("menu", "eixos"); ?>
 		</div>
 	</div>
-	<div class="mt-md row">
-		<div class="col-md-3">
-			<?php get_template_part("menu", "indice"); ?>
-			<div class="mt-md" id="fixa-menu-eixo">
-				<?php get_template_part("menu", "eixos"); ?>
-			</div>
-		</div>
-		<div class="col-md-9">
-			<h4><strong><?php the_title(); ?></strong></h4>
-			<div class="commentable-container">
-				<?php the_content(); ?>
-			</div>
+	<div class="col-md-9">
+		<h4><strong><?php the_title(); ?></strong></h4>
+		<div class="commentable-container">
+			<?php the_content(); ?>
 		</div>
 	</div>
 </div>
-<?php get_template_part('modalteste'); ?>
+<?php
+// Gera os modais de eixos
+$query_eixos = new WP_Query("post_type=eixo-de-debate");
+
+if ( $query_eixos->have_posts() ) {
+	while ( $query_eixos->have_posts() ) {
+		$query_eixos->the_post();
+		get_template_part('modal', 'eixo');
+	}
+}
+wp_reset_query();
--- single-texto-em-debate.php
+++ single-texto-em-debate.php
@@ -1,15 +1,20 @@
 <?php
 get_header(); ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <?php get_template_part("menu", "horizontal"); ?>
+        </div>
+    </div>
     <?php
         // Start the Loop.
         while ( have_posts() ) : the_post();
 
             // Include the page content template.
-            get_template_part( 'content', 'texto-em-discussao' );
+            get_template_part( 'content', 'texto-em-debate' );
 
         endwhile;
     ?>
-
+</div>
 <?php
 get_footer();
