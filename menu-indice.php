860bae35c4adfe80f6d775416428723e545aadf0
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-25 18:41:06 -0200

    alterações no submenu índice, e no no botão back-to-top;

--- menu-indice.php
+++ menu-indice.php
@@ -1,8 +1,10 @@
 <div class="menu-submenu indice">
 	<div class="panel panel-submenu">
-		<div class="panel-heading"><h6 class="panel-title">Índice</h6></div>
+		<div class="panel-heading clearfix"><h6 class="panel-title pull-left">Índice</h6> <button type="button" class="btn btn-default btn-xs fecha-menu pull-right" title="Econder este menu"><i class="fa fa-angle-up"></i></button></div>
 		<div class="panel-body vertical">
-			<?php echo dadospessoais_get_toc(get_the_content());  ?>
+			<div class="menu">
+				<?php echo dadospessoais_get_toc(get_the_content());  ?>
+			</div>
 		</div>
 	</div>
 </div>
