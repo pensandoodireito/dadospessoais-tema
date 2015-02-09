c0c5f90d01ff602206ce081cbd3bcb45616641de
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-02-08 23:19:29 -0200

    criação de box com o link para o texto de lei dentro do eixo

---
+++ content-eixo-de-debate.php
@@ -0,0 +1,16 @@
+<div class="container content-noticia">
+	<div class="row">
+    <h4 class="red"><strong><?php the_title(); ?></strong></h4>
+        <div class="col-xs-10">
+            <?php the_content(); ?>
+        </div>
+        <div class="col-xs-2">
+            <?php
+            $page_apl = dadospessoais_get_by_slug('anteprojeto-de-lei-para-a-protecao-de-dados-pessoais', OBJECT, 'texto-em-debate');
+            ?>
+            <a href="<?php echo get_permalink($page_apl->ID); ?>">
+                Visualizar esse eixo no texto de lei
+            </a>
+        </div>
+    </div>
+</div>
