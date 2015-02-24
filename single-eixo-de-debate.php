8d87e5e60df4bd4ecc4c57a9a0525bb304ea3835
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-02-23 23:25:16 -0300

    modificação do texto do campo de comentário para deixar claro a possibilidade de deixar opiniões também dentro dos eixos de debate

--- single-eixo-de-debate.php
+++ single-eixo-de-debate.php
@@ -9,7 +9,13 @@ get_header(); ?>
         while (have_posts()) : the_post();
             // Include the page content template.
             get_template_part('content', 'eixo-de-debate');
-            comments_template();
+            comment_form(array(
+                'logged_in_as'         => '<p class="red">A sua participação pode ser através desse campo de comentários para opinar sobre os aspectos do Anteprojeto dentro desse eixo.</p>
+                <p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), get_edit_user_link(), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</p>',
+                'comment_notes_before' => '<p class="red">A sua participação pode ser através desse campo de comentários para opinar sobre os aspectos do Anteprojeto dentro desse eixo.</p>
+                                            <p class="comment-notes"><span id="email-notes">' . __( 'Your email address will not be published.' ) . '</span>'. ( $req ? $required_text : '' ) . '</p>',
+                'title_reply'          => 'Escreva sua opinião',
+            ));
         endwhile;
         ?>
     </div>
