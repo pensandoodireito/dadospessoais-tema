65b89bdf86833a493d8fcfcdffd147b3a92d524f
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-03-16 18:36:09 -0300

    correção de notices no tema de dados pessoais

--- single-eixo-de-debate.php
+++ single-eixo-de-debate.php
@@ -11,9 +11,9 @@ get_header(); ?>
             get_template_part('content', 'eixo-de-debate');
             comment_form(array(
                 'logged_in_as'         => '<p class="red">A sua participação pode ser através desse campo de comentários para opinar sobre os aspectos do Anteprojeto dentro desse eixo.</p>
-                <p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), get_edit_user_link(), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</p>',
+                <p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), get_edit_user_link(), $user_identity, wp_logout_url( get_the_permalink() ) ) ) . '</p>',
                 'comment_notes_before' => '<p class="red">A sua participação pode ser através desse campo de comentários para opinar sobre os aspectos do Anteprojeto dentro desse eixo.</p>
-                                            <p class="comment-notes"><span id="email-notes">' . __( 'Your email address will not be published.' ) . '</span>'. ( $req ? $required_text : '' ) . '</p>',
+                                            <p class="comment-notes"><span id="email-notes">' . __( 'Your email address will not be published.' ) . '</span>'. ( isset($req) ? $required_text : '' ) . '</p>',
                 'title_reply'          => 'Escreva sua opinião',
             ));
         endwhile;
