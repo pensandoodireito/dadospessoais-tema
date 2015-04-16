254d87e183cbd49484459f63b897ed4f0ed8a181
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-04-16 11:37:45 -0300

    Inverter box de eixos com índice. Apresentar box de eixos colapsado (botao recolher);

--- menu-indice.php
+++ menu-indice.php
@@ -1,6 +1,6 @@
-<div class="menu-submenu indice">
+<div class="menu-submenu indice"  id="menueixos-sticker">
 	<div class="panel panel-submenu">
-		<div class="panel-heading clearfix"><h6 class="panel-title pull-left">Índice</h6> <button type="button" class="btn btn-default btn-xs fecha-menu pull-right" title="Econder este menu"><i class="fa fa-angle-up"></i></button></div>
+		<div class="panel-heading clearfix"><h6 class="panel-title pull-left">Índice</h6> <button type="button" class="btn btn-default btn-xs fecha-menu-indice pull-right" title="Econder este menu"><i class="fa fa-angle-up"></i></button></div>
 		<div class="panel-body vertical">
 			<div class="menu">
 				<?php echo dadospessoais_get_toc(get_the_content());  ?>
