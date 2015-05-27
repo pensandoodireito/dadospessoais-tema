acef69946dbbab89aabebb3d33d42d0d337d0c8f
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-05-27 13:09:56 -0300

    remoção da lista de PDF da lista de commentables e ajuste no action do form de envio (bugs no teste da master). Referente a pensandoodireito/participacao-sitebase#133 e pensandoodireito/participacao-sitebase#139

--- content-texto-em-debate.php
+++ content-texto-em-debate.php
@@ -9,32 +9,27 @@
 		<h4><strong><?php the_title(); ?></strong></h4>
 		<div class="commentable-container">
 			<?php the_content(); ?>
-
+		</div>
+		<div>
 			<h4 class="divider-top red font-roboto mt-lg pt-lg">Contribuições em PDF</h4>
 			<!-- data-section-id iniciando em um valor alto para não conflitar com os comentários do texto -->
 			<?php
 			$pdf_contribution_list = get_post_meta(get_the_ID(), 'pdf_contribution_list', true);
 
-			$data_section = 10000;
-
-			foreach ($pdf_contribution_list as $pdf_contribution) {
-
-			?>
-			<p class="commentable-section" data-section-id="<?php echo $data_section; ?>">
+			foreach ($pdf_contribution_list as $pdf_contribution) {	?>
+			<p class="md-lg pt-lg">
 				<img alt="pdf" src="<?php echo get_stylesheet_directory_uri();?>/images/pdf-icon.png"/><strong><?php echo $pdf_contribution['author']; ?></strong><br/>
 				<a href="<?php echo $pdf_contribution['pdf_url']; ?>">Clique para baixar a contribuição em PDF</a>
-        <br/>
 			</p>
-			<?php
-			$data_section++;
-
-			} ?>
+			<?php } ?>
+			<p class="md-lg pt-lg">
+			</p>
 		</div>
 		<div class="well">
 			<h5><strong>Envie sua contribuição em PDF</strong></h5>
 
 			<?php if (is_user_logged_in()) { ?>
-      <form method="POST" id="form-contribuicao-pdf" action="<?php echo $_SERVER['PHP_SELF'] ?>#form-contribuicao-pdf" enctype="multipart/form-data">
+      <form method="POST" id="form-contribuicao-pdf" action="<?php echo $_SERVER['REQUEST_URI']; ?>#form-contribuicao-pdf" enctype="multipart/form-data">
 				<div class="row">
 					<div class="col-sm-8 mt-sm">
             <input type="hidden" name="file_nonce" value="<?php echo wp_create_nonce('pdf-upload'); ?>" />
